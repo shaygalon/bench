2ddcce5b966c8733d6474f6587363b8875caaee3
Shay Gal-On <sgalon@cavium.com>
2017-09-22 13:14:29 -0700

    add average for overall perf

--- csv_to_perf.pl
+++ csv_to_perf.pl
@@ -27,13 +27,20 @@ if ($hl =~ /Unit/i) { $has_unit=1; } else { $has_unit=0; }
  
 # Add elements to the document
 my $header = $xml->add('test' => { 'name' => $FN, 'executed' => 'yes' } );
-my $r=$header->add('result' => {} );
-my $m=$r->add('metrics' => {} );
+my $r=$header->add('result' );
+$r->add('success' => { 'passed' => "yes", 'state' => "100", 'hasTimedOut' => "false" } ); 
+$m=$r->add('metrics' => {} );
+$total=0; $count=0;
 for $fl (@r) {
 	@f=split /,/,$fl;
+	next if (@f<3);
 	if ($has_unit) { $unit=$f[1]; } else { $unit="A"; }
 	$m->add($f[0] => { 'unit' => $unit, 'mesure' => $f[2], 'isRelevant' => "yes" } );
+	$total+=$f[2];
+	$count++;
 }
+$r->add('performance' => { 'unit' => 'avg', 'mesure' => $total/$count, 'isRelevant' => "yes" } );
+
 print $xml->to_pretty_xml;
 
 
